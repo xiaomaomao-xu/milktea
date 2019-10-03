4c33b261ef1a0fd013af73cb12937b29ed18ef78
dengshidongai <1249277931@qq.com>
2019-10-03 16:13:20 +0800

    # WARNING: head commit changed in the meantime
    cheshi1

---
+++ milk-tea/src/main/java/com/hsys/tea/milktea/socket/HandshakeInterceptor.java
@@ -0,0 +1,38 @@
+package com.hsys.socket;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.http.server.ServletServerHttpRequest;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
+
+public class HandshakeInterceptor extends HttpSessionHandshakeInterceptor{
+
+
+    // 握手前
+    @Override
+    public boolean beforeHandshake(ServerHttpRequest request,
+            ServerHttpResponse response, WebSocketHandler wsHandler,
+            Map<String, Object> attributes) throws Exception {
+    	HttpServletRequest servletRequest = ((ServletServerHttpRequest) request).getServletRequest();
+        String type = servletRequest.getParameter("user");
+        attributes.put("user", type);
+        return super.beforeHandshake(request, response, wsHandler, attributes);
+    }
+
+
+
+    // 握手后
+    @Override
+    public void afterHandshake(ServerHttpRequest request,
+            ServerHttpResponse response, WebSocketHandler wsHandler,
+            Exception ex) {
+        super.afterHandshake(request, response, wsHandler, ex);
+    }
+
+}
---
+++ milk-tea/src/main/java/com/hsys/tea/milktea/socket/MyWebSocketHandler.java
@@ -0,0 +1,91 @@
+package com.hsys.socket;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.WebSocketMessage;
+import org.springframework.web.socket.WebSocketSession;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+public class MyWebSocketHandler implements WebSocketHandler{
+	private static ConcurrentHashMap<String, WebSocketSession> webSocketSet = new ConcurrentHashMap<String, WebSocketSession>();//用户
+	private static ConcurrentLinkedQueue<WebSocketSession> webSocketSetcat = new ConcurrentLinkedQueue<WebSocketSession>();//用户
+	private static ConcurrentHashMap<String, WebSocketSession> webSocketSetjiu = new ConcurrentHashMap<String, WebSocketSession>();//用户
+    // 连接 就绪时 
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session)
+            throws Exception {
+    	String userId = (String) session.getHandshakeAttributes().get("user");
+//    	System.out.println(userId);
+    	if(userId.split(",")[0].equals("c")){
+    		webSocketSetcat.add(session);
+    	}else if(userId.split(",")[0].equals("s")) {
+    		webSocketSetjiu.put(userId, session);
+    	}else {
+    		webSocketSet.put(userId,session);
+    	}
+    }
+    // 处理信息
+    @Override
+    public void handleMessage(WebSocketSession session,
+         WebSocketMessage<?> message) throws Exception {
+    	 Gson gson = new Gson();
+    	 Map<String, Object> msg = gson.fromJson(message.getPayload().toString(),new TypeToken<Map<String, Object>>() {}.getType());
+//    	 System.out.println("后台："+msg.get("msgContent").toString());
+    	 if(msg.get("msgContent").toString().split(",")[0].equals("c")) {
+    		 sendMsgToAllUsers(message);
+    	 }else if(msg.get("msgContent").toString().split(",")[0].equals("s")) {
+    		 try {
+				WebSocketSession gsp = webSocketSetjiu.get(msg.get("gspId").toString());
+				gsp.sendMessage(message);
+			} catch (Exception e) {
+			}
+    	 }
+    }
+
+
+    // 处理传输时异常
+    @Override
+    public void handleTransportError(WebSocketSession session,
+            Throwable exception) throws Exception {
+        // TODO Auto-generated method stub
+
+    }
+
+
+
+    // 关闭 连接时
+    @Override
+    public void afterConnectionClosed(WebSocketSession session,
+            CloseStatus closeStatus) throws Exception {
+
+    	String userId = (String) session.getHandshakeAttributes().get("user");
+    	webSocketSet.remove(userId);
+
+    }
+
+
+
+    @Override
+    public boolean supportsPartialMessages() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+
+
+    // 给所有用户发送 信息
+    public void sendMsgToAllUsers(WebSocketMessage<?> message) throws Exception{
+
+        for(WebSocketSession car:webSocketSetcat) {
+        	car.sendMessage(message);
+        }
+    }
+
+}
