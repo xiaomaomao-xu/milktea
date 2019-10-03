612d02394269b06ae26486e5fb5a5b89c55b48db
xiaomaomao-xu <xiaomaomxh@163.com>
2019-10-03 15:50:02 +0800

    shanchumeideyongde

--- milk-tea/src/main/java/com/hsys/tea/milktea/websocket/mytext.java
+++
@@ -1,9 +0,0 @@
-package com.hsys.tea.milktea.websocket;
-
-public class mytext {
-
-	public static void main(String[] args) {
-		System.out.println("你好");
-		System.out.println("你好 发");
-	}
-}
--- milk-tea/src/main/java/com/hsys/tea/milktea/websocket/xicat.java
+++
@@ -1,7 +0,0 @@
-package com.hsys.tea.milktea.websocket;
-
-public class xicat {
-public static void main(String[] args) {
-	System.out.println("hll");
-}
-}
