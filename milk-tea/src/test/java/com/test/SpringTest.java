0bbef00a1fcca88b39fde53d4bdeb3830a3685a5
xiaomaomao-xu <xiaomaomxh@163.com>
2019-10-04 14:45:06 +0800

    tijiaotestwenjian-xuhuan

--- milk-tea/src/test/java/com/test/SpringTest.java
+++ milk-tea/src/test/java/com/test/SpringTest.java
@@ -1,6 +1,27 @@
 package com.test;
 
+import java.util.List;
 
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.hsys.tea.milktea.entity.Storeinfo;
+import com.hsys.tea.milktea.user.service.IndexService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:spring.xml"})
 public class SpringTest {
+	@Autowired
+	private IndexService is;
 	
+	@Test
+	public void test1() {
+		List<Storeinfo> findAllStoreinfos = is.findAllStoreinfos();
+		for(Storeinfo si : findAllStoreinfos) {
+			System.out.println(si.getBusinessHours());
+		}
+	}
 }
